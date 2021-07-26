be15255534e524f67d55fddb0111c4b2028afccb
CedArctic <kdoumani98@gmail.com>
2021-07-26 12:55:35 +0300

    Broker Node: - broker.js: Broker properties are now in msg["brokers"][broker_name]

--- broker.js
+++ broker.js
@@ -4,17 +4,17 @@ module.exports = function(RED) {
         let initTimeout = 500;
         RED.nodes.createNode(this, config);
         const node = this;
-        const msg = {
-            payload: {
-                name: this.name,
-                host: config.host,
-                port: config.port,
-                b_type: config.b_type,
-                username: config.username,
-                password: config.password,
-                db: config.db,
-                exchange: config.exchange
-            }
+        // Add broker properties into message
+        const msg = {brokers: {}}
+        msg["brokers"][this.name] = {
+            name: this.name,
+            host: config.host,
+            port: config.port,
+            b_type: config.b_type,
+            username: config.username,
+            password: config.password,
+            db: config.db,
+            exchange: config.exchange
         }
         // Send message after initial delay
         setTimeout(() => node.send(msg), initTimeout);
