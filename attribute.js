10ffe3590476cc8b099e2ec92065e0d8001a1cbd
CedArctic <kdoumani98@gmail.com>
2021-07-26 12:39:19 +0300

    Added Attribute Node

---
+++ attribute.js
@@ -0,0 +1,17 @@
+module.exports = function(RED) {
+    function AttributeNode(config) {
+        // Artificial timeout on deployment so that all nodes can be initialized.
+        let initTimeout = 500;
+        RED.nodes.createNode(this, config);
+        const node = this;
+        const msg = {
+            payload: {
+                name: this.name,
+                a_type: config.a_type
+            }
+        }
+        // Send message after initial delay
+        setTimeout(() => node.send(msg), initTimeout);
+    }
+    RED.nodes.registerType("attribute", AttributeNode);
+}
